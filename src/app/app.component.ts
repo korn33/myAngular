76bdfae209c3ddbe0c3e73027c73137051700c2f
a.kornienko <a.kornienko@octaviangameart.ru>
2022-03-01 09:22:37 +0300

    transfer parameters between components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,20 +7,14 @@ import { IPost } from 'src/interfaces/formPosts/IPost';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  posts: IPost[] = [
-    {
-      title: "First post",
-      text: "First post text df 1111111111111 lorem dfdsgf 10"
-    },
-    {
-      title: "Second post",
-      text: "Second post dsf dsfdf dfgsdf ds text lorem dfdsgf 10"
-    },
-  ];
+  posts: IPost[] = [];
 
 
   constructor() {
 
   }
 
+  updatePosts($event: IPost) {
+    this.posts.unshift($event)
+  }
 }
--- src/app/post-form/post-form.component.ts
+++ src/app/post-form/post-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {IPost} from "../../interfaces/formPosts/IPost";
 
 @Component({
   selector: 'app-post-form',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-form.component.scss']
 })
 export class PostFormComponent implements OnInit {
+  title = ""
+  text = ""
+
+  @Output() onAdd: EventEmitter<IPost> = new EventEmitter<IPost>()
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addPost() {
+    if (this.text.trim() && this.title.trim()){
+      const post: IPost = {
+        title: this.title,
+        text: this.text
+      }
+      this.onAdd.emit(post)
+      this.text = this.title = ""
+    }
+  }
 }
