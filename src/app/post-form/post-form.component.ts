936ec6d759111226958d268714c5ee0221541e73
a.kornienko <a.kornienko@octaviangameart.ru>
2022-03-01 18:54:50 +0300

    html element ref

--- src/app/post-form/post-form.component.ts
+++ src/app/post-form/post-form.component.ts
@@ -1,30 +1,36 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
 import {IPost} from "../../interfaces/formPosts/IPost";
 
 @Component({
-  selector: 'app-post-form',
-  templateUrl: './post-form.component.html',
-  styleUrls: ['./post-form.component.scss']
+    selector: 'app-post-form',
+    templateUrl: './post-form.component.html',
+    styleUrls: ['./post-form.component.scss']
 })
 export class PostFormComponent implements OnInit {
-  title = ""
-  text = ""
+    title = ""
+    text = ""
 
-  @Output() onAdd: EventEmitter<IPost> = new EventEmitter<IPost>()
+    @Output() onAdd: EventEmitter<IPost> = new EventEmitter<IPost>()
+    @ViewChild("inputTitle") inputTitleRef!: ElementRef
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  addPost() {
-    if (this.text.trim() && this.title.trim()){
-      const post: IPost = {
-        title: this.title,
-        text: this.text
-      }
-      this.onAdd.emit(post)
-      this.text = this.title = ""
+    constructor() {
     }
-  }
+
+    ngOnInit(): void {
+    }
+
+    addPost() {
+        if (this.text.trim() && this.title.trim()) {
+            const post: IPost = {
+                title: this.title,
+                text: this.text
+            }
+            this.onAdd.emit(post)
+            this.text = this.title = ""
+        }
+    }
+
+    setFocusInputTitle() {
+        this.inputTitleRef.nativeElement.focus()
+    }
 }
