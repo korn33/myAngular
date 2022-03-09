90cde9161b39f2dac23e8ef7f51b6dc50a6dd618
korn33 <korn33.mail@gmail.com>
2022-03-09 23:09:48 +0300

    created directive

---
+++ src/app/directives/style.directive.ts
@@ -0,0 +1,12 @@
+import {Directive, ElementRef, Renderer2} from '@angular/core';
+
+@Directive({
+  selector: '[appStyleDirective]'
+})
+export class StyleDirective {
+
+  constructor(private elRef: ElementRef, private renderer: Renderer2) {
+    this.renderer.setStyle(this.elRef.nativeElement, 'color', 'blue')
+  }
+
+}
