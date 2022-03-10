5cc61bb03e0aab93a0fe2a772e13aae09bb13344
a.kornienko <a.kornienko@octaviangameart.ru>
2022-03-10 18:33:48 +0300

    add event listener

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,12 +1,19 @@
-import {Directive, ElementRef, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostListener, Renderer2} from '@angular/core';
 
 @Directive({
-  selector: '[appStyleDirective]'
+    selector: '[appStyleDirective]'
 })
 export class StyleDirective {
 
-  constructor(private elRef: ElementRef, private renderer: Renderer2) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'color', 'blue')
-  }
+    constructor(private elRef: ElementRef, private renderer: Renderer2) {
 
+    }
+
+    @HostListener('mouseenter') onMouseEnter() {
+        this.renderer.setStyle(this.elRef.nativeElement, 'color', 'blue')
+    }
+
+    @HostListener('mouseleave') onMouseLeave() {
+        this.renderer.setStyle(this.elRef.nativeElement, 'color', null)
+    }
 }
