0bf67e046c8a6ae592eeb832903e66014339d597
a.kornienko <a.kornienko@octaviangameart.ru>
2022-03-10 18:59:58 +0300

    send parameters in directive

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,9 +1,10 @@
-import {Directive, ElementRef, HostListener, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostListener, Input, Renderer2} from '@angular/core';
 
 @Directive({
     selector: '[appStyleDirective]'
 })
 export class StyleDirective {
+    @Input() dStyles!: {color?: string, border?: string, borderRadius?: string}
 
     constructor(private elRef: ElementRef, private renderer: Renderer2) {
 
@@ -14,10 +15,14 @@ export class StyleDirective {
     }
 
     @HostListener('mouseenter') onMouseEnter() {
-        this.renderer.setStyle(this.elRef.nativeElement, 'color', 'blue')
+        this.renderer.setStyle(this.elRef.nativeElement, 'color', this.dStyles.color)
+        this.renderer.setStyle(this.elRef.nativeElement, 'border', this.dStyles.border)
+        this.renderer.setStyle(this.elRef.nativeElement, 'borderRadius', this.dStyles.borderRadius)
     }
 
     @HostListener('mouseleave') onMouseLeave() {
-        this.renderer.setStyle(this.elRef.nativeElement, 'color', null)
+        this.renderer.setStyle(this.elRef.nativeElement, 'color',null)
+        this.renderer.setStyle(this.elRef.nativeElement, 'border', null)
+        this.renderer.setStyle(this.elRef.nativeElement, 'borderRadius', null)
     }
 }
