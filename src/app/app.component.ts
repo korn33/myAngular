ab4d84a7c109155ff22485cc6a4254bbe2dc88c7
korn33 <korn33.mail@gmail.com>
2022-04-24 13:24:39 +0300

    start new module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import {Component, OnInit} from '@angular/core'
-import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
-import {MyValidators} from "./MyValidators";
 
 @Component({
   selector: 'app-root',
@@ -8,56 +6,9 @@ import {MyValidators} from "./MyValidators";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-    appState = 'off';
 
-    form!: FormGroup;
+  ngOnInit() {
 
-    ngOnInit(): void {
-        this.form = new FormGroup({
-            email: new FormControl('er@y', [
-                Validators.email,
-                Validators.required,
-                MyValidators.restrictedEmail
-            ], [MyValidators.uniqEmail]),
-            password: new FormControl('123455', [Validators.required, Validators.minLength(6)]),
-            address: new FormGroup({
-                country: new FormControl('by'),
-                city: new FormControl('Самара', [Validators.required])
-            }),
-            skills: new FormArray([])
-        })
-    }
-
-    submit() {
-        if (this.form.valid) {
-            // console.log('yo! form: ', this.form);
-            console.log('controls: ', this.form.value);
-            this.form.reset();
-        }
-    }
-
-    setCapital(){
-        const cityMap: any = {
-            ru: 'Москва',
-            ua: 'Киев',
-            by: 'Минск'
-        };
-
-        const countryKey: string = this.form.get('address')?.get('country')?.value;
-        const city = cityMap[countryKey];
-        this.form.patchValue({address: {city}})
-    }
-
-    addSkill() {
-        const control = new FormControl('', Validators.required);
-        (<FormArray>this.form.get('skills')).push(control);
-    }
-
-    getControls() {
-        return (this.form.get('skills') as FormArray).controls
-    }
-
-    handleChange() {
-        console.log(this.appState)
-    }
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,18 @@ import {BrowserModule} from '@angular/platform-browser'
 import {NgModule} from '@angular/core'
 
 import {AppComponent} from './app.component'
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { SwitcherComponent } from './switcher/switcher.component'
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        SwitcherComponent,
-    ],
-    imports: [
-        BrowserModule,
-        FormsModule,
-        ReactiveFormsModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 }
