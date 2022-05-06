3b716def0aee0011803277f15daf6698e4d6a8c3
korn33 <korn33.mail@gmail.com>
2022-05-06 19:47:40 +0300

    get

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,31 @@
 import {Component, OnInit} from '@angular/core'
+import {HttpClient} from "@angular/common/http";
+
+export interface ITodo {
+  id: number,
+  completed: boolean,
+  title: string
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
 export class AppComponent implements OnInit {
 
-  ngOnInit() {
+  constructor(private http: HttpClient) {}
+  
+  todos: ITodo[] = [];
 
+  ngOnInit() {
+    this.http.get<ITodo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2').subscribe(todos =>{
+      console.log('params: ', todos);
+
+      this.todos = todos;
+    })
   }
 }
 
