30756609301ebe8848a62f186857d1601f539ebb
korn33 <korn33.mail@gmail.com>
2022-05-29 09:11:43 +0300

    create new src for routing module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,61 +1,11 @@
-import {Component, OnInit} from '@angular/core'
-import {ITodo, TodosService} from "./Todos.service";
+import {Component} from '@angular/core'
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
-
-export class AppComponent implements OnInit {
-    constructor(private todosService: TodosService) {}
-
-    todos: ITodo[] = [];
-    todoTitle: string = '';
-    isLoading: boolean = false;
-
-    errorMessage: string = '';
-
-    ngOnInit() {
-       this.onLoad()
-    }
-
-    onAdd() {
-        if (!this.todoTitle.trim()) return
-        const newTodo: ITodo = {
-            id: 0,
-            completed: false,
-            title: this.todoTitle
-        };
-
-        this.todosService.Add(newTodo).subscribe(todo => {
-            this.todos.push(todo)
-        })
-    }
-
-    onLoad() {
-        this.isLoading = true;
-        this.todosService.Load()
-            .subscribe(todos => {
-            console.log('params: ', todos);
-            this.todos = todos;
-            this.isLoading = false;
-        })
-    }
-
-    onDelete(id: number) {
-        this.todosService.Delete(id).subscribe(() => {
-            this.todos = this.todos.filter(value => value.id !== id)
-        })
-    }
-
-    onComplete(id: number) {
-        this.todosService.Complete(id).subscribe(todo => {
-            const completingTodo = this.todos.find(value => value.id === todo.id);
-            if (completingTodo) completingTodo.completed = todo.completed
-        }, error => {
-            this.errorMessage = error.message
-        })
-    }
+export class AppComponent {
+  constructor() {}
 }
 
