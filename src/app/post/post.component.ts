9a193c00ce738f101dd3649e3de08fef51cab520
a.kornienko <a.kornienko@octaviangameart.ru>
2022-05-30 19:08:29 +0300

    subscribe on the url changed

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,10 +1,34 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core'
+import {Post, PostsService} from "../posts.service";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss']
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
+    post: Post = {
+        title: "",
+        text: "",
+        id: 0
+    };
 
+    constructor(
+        private activatedRoute: ActivatedRoute,
+        private postsService: PostsService,
+        private router: Router
+    ) {
+
+    }
+
+    ngOnInit() {
+        this.activatedRoute.params.subscribe((value: Params) => {
+           this.post = this.postsService.getById(+value['id']) as Post;
+        })
+    }
+
+    load4Post() {
+        this.router.navigate(['/posts', 44])
+    }
 }
